86d47788aeb1e658f151657fea4232b2014e2c8d
SylvainTorrenti <torrsyl@gmail.com>
2023-01-17 15:32:03 +0100

    Ajoutez des fichiers projet.

---
+++ TpCompteHeritage/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpCompteHeritage
+{
+    internal class Account
+    {
+        private static int _accountNb = 0;
+        private double _balance;
+
+        public static int AccountNb { get => _accountNb; private set => _accountNb = value; }
+        public double Balance { get => _balance; private set => _balance = value; }
+
+        public Account()
+        {
+            AccountNb += 1;
+            Balance = 0;
+        }
+
+        public Account(int balance)
+        {
+            Balance = balance;
+            AccountNb += 1;
+        }
+
+        public double Credit(double amount)
+        {
+            return Balance = amount;
+        }
+        public double Deposit(double amount) { }
+    }
+}
